df8ffe367b63e9a5c6ea2f7133b137f02b11ec69
Luiz Gustavo Setten <consultor.119@7comm.com.br>
2021-09-01 18:13:12 -0300

    feat: insert in database

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,29 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
+
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  let isUserLoggedIn = true;
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
-      <FaGithub color="#04d361"/> 
-      Luiz Setten
+  let [session] = useSession();
+
+  return session ? (
+    <button 
+      type="button" 
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
+      {/* <FaGithub color="#04d361"/>  */}
+      <img src={session.user.image} alt={session.user.name} width="24px" height="24px" style={{ borderRadius: "50%", marginRight: "1rem"}}/>
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon}/>
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button 
+      type="button" 
+      className={styles.signInButton} 
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="#eba417"/> 
       Sign in with GIthub
     </button>
